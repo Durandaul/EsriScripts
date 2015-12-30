d10d78baac5ae07872d088786120badc101c09c8
unknown <mhu@DESKTOP-IN2H4LK.tempero.local>
2015-12-30 10:33:40 -0800

    Updated with most recent changes from Server

--- lock_identifier.py
+++ lock_identifier.py
@@ -1,80 +1,90 @@
 '''
-A script to identify all lock files and their paths.
+A script to identify all lock files and their paths. Currently this is optimized for:
+1) Python 2.7 (print statements will break in 3)
+2) Windows. No unix/mac functionality is exclusive but pathing and interpretation of path is explicitly windows
+3) To only use python 2.7 standard library
+To be Added: 
+    - Options for file metrics
+    -CSV Output
+    -More generalization with esri specific class
 '''
 import os   # Interact with windows files and folders
-import csv
 import argparse
-from pprint import pprint as pp
 import shelve
+from pprint import pprint as pp
 
-def walk(drive, directory, fileExt, verbose=False):
-    ''' Perform Walk from (A) Drive and (B) Directory to look for (C) Files '''
+def createDataObject(drive, directory):
+    ''' Create a dictionairy with the path to be used for the initial walk'''
+    dataObject = {}  
+    pathToStr ="{drive}:\{directory}".format(drive=drive, directory=directory) 
+    if verbose:
+        print pathToStr
+    os.chdir(pathToStr)
+    if verbose:
+        print os.getcwd()
+    return dataObject['INIT_PATH']=pathToStr
+
+
+def walk(dataStore, fileExt, verbose=False):
+    ''' Perform Walk from (A) Drive and (B) Directory to look for (C) Files. The return value from walk is a 
+    3 tuple that contains dirpath[0], dirnames[1], and filenames[2].
+    The return object will have:
+        -The key INIT_PATH whcih is the start path
+        - A key for each folder walked containg a list comprehension of matching files to extensions. '''
     if verbose:
         print "Starting Walk"
-    walkData = {}  # Data Object to return
 
-    pathToStr ="{drive}:\{directory}".format(drive=drive, directory=directory)   #Create formatted string 
- 
-    if verbose== True:
-        print pathToStr
+    dirWalk = os.walk(dataStore['INIT_PATH'])
 
-    os.chdir(pathToStr)    #Create a Windows specific directory string. Need to test if this happened
-
-    walk = os.walk(pathToStr)
-    for tupData in walk: #Tuple tupData :0 == dirpath ; 1 == dirnames ; 2 == filenames
-        
+    for tupData in dirWalk:
         for fname in tupData[2]: # For Each string in the tuple for filenames
-            if fileExt in fname.lower():
-                walkData[str(tupData[0])] = [fname for fname in tupData[2] if fileExt.lower() in fname.lower() ] 
-                #take a string of the directory path as the key and make the value the file name extension
+            if fileExt in fname.lower(): #If the file extension
+                dataStore[str(tupData[0])] = [fname for fname in tupData[2] if fileExt.lower() in fname.lower() ]
+                if verbose:
+                    print "{path}:{fileName}".format(path=tupData[0],fileName=fname.lower())
 
     return walkData
 
-def lockFileParser(lockFile):
-    ''' Pass a lockfilename into the parser to seperate out into process, misc, file, directory, and computer'''
-    lockParse=lockFile.split('.') 
-    try:
-        lockOut=dict(computer=lockParse[1], processID=lockParse[3],fType=lockParse[5],fName=lockFile)
-    except IndexError:
-        lockOut=dict(fName=lockFile)
-    return lockOut
+def outputfunction(walkResults, out):
+    ''' Choice of output is currently prettyprint, shelve, or dictionairy. Dictionairy is default'''
+    if walkResults:
+        if out == 'pp':
+            pp(walkResults)
 
-def parseData(walkOutput):
-    
-    walkResultsHolder=[]
-    for dirPath in walkOutput:
-        
-        for lockFile in walkOutput[dirPath]:
-            try:
-                walkResults={}
-                lockOut=lockFileParser(lockFile)
-                
-                walkResults['path'] = dirPath
-                walkResults['computer'] = lockOut['computer']
-                walkResults['fname']=lockOut['fName']
-                walkResults['processID']=lockOut['processID']
-                walkResults['fqdn']="{dirPath}:\{lockFile}".format(dirPath=dirPath, lockFile=lockOut['fName'])
-            except KeyError:
-                pass
-            
-            walkResultsHolder.append(walkResults)
-            
-    return walkResultsHolder
+        elif out == 'shelve':
+            db = shelve.open('lock_out')
+            db['data'] = walkResults
+            print 'Saving to Disk as a shelve file'
+            db.close()
 
-def outputfunction(walkResults, output):
-    if out == 'pp':
-        pp(walkResults) #Fix to be pretty print
-    elif out =='dict':
-        print walkResults
+        else:
+            return walkResults
     else:
-        return walkresults
+        return walkResults
+        
+def fRemover(fPath):
+    ''' File remover function for search and remove. Not fully automated, this requires user input to ensure no mistakes are made'''
+    print "File Path to be deleted: {fpath}".format(fpath=fpath)
+    confirm=raw_input('Are you sure you want to delete this path? (y|n)')
+
+    while(confirm.lower() != 'y' or confirm.lower() != 'n'):
+        confirm=raw_input('Are you sure you want to delete this path? (y|n)')
+    if confirm.lower() == 'y':
+        os.remove(fpath)
+        return True
+    elif confirm.lower() == 'n':
+        return False
+
 
 
 def main(drive, directory, fileExt, output, verbose):
+    ''' Create a data object, pass it and the file extention to perform a walk and send to the output function'''
     if verbose:
         print "Starting Main"
-    walkResults=walk(drive, directory, fileExt, verbose)
+    dataObj = dataStore(drive,directory)
+    walkResults = walk(dataObj, fileExt, verbose) 
     outputfunction(walkResults, output)
+    #Output function should be fixed as an object that returns dict or something
 
 
 
@@ -107,7 +117,13 @@ if __name__ == '__main__':
 
     parser.add_argument('-v',
                         action="store_true",
+                        #type=bool,
+                        #default=False,
                         help="Output misc for debugging")
+
+    parser.add_argument('-d',
+                        action='store_true',
+                        help="Invoke the deleter function and manually go through a list of values to remove")
 
 
     args = parser.parse_args()
